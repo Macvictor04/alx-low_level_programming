18490f08be095f92a84f05b26b12b2d030fd692e
macvictor04 <chiwuezemacvictor@gmail.com>
2023-06-25 08:42:20 -0700

    bad

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return:0
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n < 10)
+	{
+		putchar(n);
+	      n++;
+	}
+	      putchar('\n');
+
+	      return (0);
+
+}
